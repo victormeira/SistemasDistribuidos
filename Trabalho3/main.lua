fbceb9496bfe67070d60252863ed671e0541b506
Victor Meira Pinto <victormeirapinto@icloud.com>
2019-06-04 18:51:46 -0300

    finished game portion

--- Trabalho3/main.lua
+++ Trabalho3/main.lua
@@ -1,12 +1,80 @@
 local mqtt = require("mqtt_library")
 
+function splitString(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 function mqttCallback(topic, message)
-  
- print(topic .."-".. message)
+
+  print(topic .."-".. message)
+
+  -- acknoledge new player
   if topic == "ack1420626" and message ~= objects.player.name then
+    
+    -- checks if we already have acknoledged the player
+    for k,v in ipairs(objects.enemies) do
+      if(message == v.name) then
+        return
+      end
+    end
+    
     mqtt_client:publish("ack1420626", objects.player.name)
-    loadNewEnemy(message)  
-  end  
+    loadNewEnemy(message)
+
+    -- update score
+  elseif topic == "score1420626" then
+    local tokens = splitString(message, ",")
+    local pName = tokens[1]
+    local pScore = tokens[2]
+    print(pName)
+    print(pScore)
+    if pName == objects.player.name then
+      objects.player.score = pScore
+    else
+      for k,v in ipairs(objects.enemies) do
+        if(pName == v.name) then
+          v.score = pScore
+          print("im here")
+        end
+      end
+    end
+
+    -- start game for everyone
+  elseif topic == "start1420626" then
+    startGame()
+  end
+  
+end
+
+function startGame()
+  if(gameState == "end") then
+    reloadGame()
+  end
+  
+  gameState = "playing"
+  start = love.timer.getTime() -- start 30 sec timer
+  missing = 30
+
+
+end
+
+function reloadGame()
+    
+  objects.player.score = 0
+
+  --spawning coins
+  objects.coins = {}
+
+  for i=1, 5 do
+    loadNewCoinLocation(i)
+  end
 end
 
 function loadNewEnemy(name)
@@ -15,7 +83,7 @@ function loadNewEnemy(name)
   en.h = 10
   en.score = 0
   en.name = name
-  
+
   table.insert(objects.enemies, en)
 end
 
@@ -38,23 +106,31 @@ function loadNewCoinLocation (i)
   local rCoin = love.math.random(3,10)
 
   objects.coins[i] = {}
-  objects.coins[i].body = love.physics.newBody(world, xCoin, yCoin, "static")
-  objects.coins[i].shape = love.physics.newCircleShape(rCoin)
-  objects.coins[i].fixture = love.physics.newFixture(objects.coins[i].body, objects.coins[i].shape)
-  objects.coins[i].fixture:setUserData("coin")
-  objects.coins[i].fixture:setCategory(2)
-  objects.coins[i].fixture:setGroupIndex(i)
-  objects.coins[i].fixture:setMask(3)
+  objects.coins[i].x = xCoin
+  objects.coins[i].y = yCoin
+  objects.coins[i].r = rCoin
 
 end
 
-function beginContact(a, b, coll)
-  if a:getUserData() == "player" and b:getUserData() == "coin" then
-    objects.player.score = objects.player.score + 100*b:getShape():getRadius()
-    table.insert(coinsToLoad,b:getGroupIndex())
-    b:setMask(1)
+function checkCoinCollision()
+  local xp = objects.player.body:getX()
+  local yp = objects.player.body:getY()
+  local wp = 10
+  local hp = 25
+  
+  for k,v in ipairs(objects.coins) do
+    local xc = v.x
+    local yc = v.y
+    local rc = v.r
+    
+    if (xp + wp/2) > (xc - rc) and (xp - wp/2) < (xc + rc) and (yp + hp/2) > (yc - rc) and (yp - hp/2) < (yc + rc) then
+      loadNewCoinLocation(k)
+      local publishedScore = objects.player.score + 100*v.r
+      mqtt_client:publish("score1420626", objects.player.name .. "," .. publishedScore)
+    end
   end
 end
+
 
 function love.keypressed(key)
   if key == "escape" then
@@ -63,23 +139,25 @@ function love.keypressed(key)
     initImpulse = objects.player.body:getMass()*400
     objects.player.body:applyLinearImpulse(0,-initImpulse)
   end
-      
+
   if gameState == "start" and key ~= "return" and not fixedName then
     objects.player.name = objects.player.name .. key
   end
-  
+
   if gameState == "start" and key == "return" then
     fixedName = true
     mqtt_client:connect("victor" .. love.math.random(1,50000))
     print("CONNECTED")
-    mqtt_client:subscribe({"ack1420626"})-- "start1420626", "coin1420626", "score1420626", "position1420626"})
+
+    connected = true
+    mqtt_client:subscribe({"ack1420626", "start1420626", "score1420626"})
     print("SUBBED")
 
     mqtt_client:publish("ack1420626", objects.player.name)
     print("PUBLISHED")
 
   end
-  
+
 end
 
 function love.load()
@@ -88,8 +166,8 @@ function love.load()
 
   gameState = "start"
   fixedName = false
+  connected = false
 
-  coinsToLoad = {}
 
   love.math.setRandomSeed(love.timer.getTime())
   love.physics.setMeter(20)
@@ -97,19 +175,19 @@ function love.load()
   world:setCallbacks(beginContact)
 
   objects = {}
-  
+
   objects.enemies = {}
 
   objects.ground = {}
-  objects.ground.body = love.physics.newBody(world, 650/2, 650-25/2) 
-  objects.ground.shape = love.physics.newRectangleShape(650, 25) 
-  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape); 
+  objects.ground.body = love.physics.newBody(world, 650/2, 650-25/2)
+  objects.ground.shape = love.physics.newRectangleShape(650, 25)
+  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape);
 
   --let's create a player
   objects.player = {}
   objects.player.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
   objects.player.shape = love.physics.newRectangleShape(10, 25)
-  objects.player.fixture = love.physics.newFixture(objects.player.body, objects.player.shape, 1) 
+  objects.player.fixture = love.physics.newFixture(objects.player.body, objects.player.shape, 1)
   objects.player.fixture:setUserData("player")
   objects.player.fixture:setCategory(1)
   objects.player.score = 0
@@ -127,9 +205,9 @@ function love.load()
     objects.platforms[i] = {}
     objects.platforms[i].w = w
     objects.platforms[i].h = h
-    objects.platforms[i].body = love.physics.newBody(world, xPos, yPos) 
-    objects.platforms[i].shape = love.physics.newRectangleShape(w, h) 
-    objects.platforms[i].fixture = love.physics.newFixture(objects.platforms[i].body, objects.platforms[i].shape); 
+    objects.platforms[i].body = love.physics.newBody(world, xPos, yPos)
+    objects.platforms[i].shape = love.physics.newRectangleShape(w, h)
+    objects.platforms[i].fixture = love.physics.newFixture(objects.platforms[i].body, objects.platforms[i].shape);
   end
 
   --spawning coins
@@ -154,30 +232,31 @@ end
 
 
 function love.update(dt)
-  
+
+  if connected then
+    mqtt_client:handler()
+  end
+
+
   if gameState == "start" or gameState == "end" then
     if(love.keyboard.isDown("space")) then
-      gameState = "playing"
-      start = love.timer.getTime() -- start 30 sec timer
-      missing = 30
-      
-      if(gameState == "end") then
-        love.load()
-      end
+      mqtt_client:publish("start1420626", "start")
+
     end
   else
-    world:update(dt) 
+    world:update(dt)
+    checkCoinCollision()
     missing = 30 - (love.timer.getTime() - start)
-    
+
     if(missing < 0) then
-        gameState = "end"
+      gameState = "end"
     end
 
     posX = objects.player.body:getX()
     velX, velY = objects.player.body:getLinearVelocity()
     objects.player.body:setAngle(0) -- avoid object rotation
     --here we are going to create some keyboard events
-    if love.keyboard.isDown("right") then 
+    if love.keyboard.isDown("right") then
       posX = posX + 200*dt
 
       -- borders to the window
@@ -186,7 +265,7 @@ function love.update(dt)
       end
 
       objects.player.body:setX(posX)
-    elseif love.keyboard.isDown("left") then 
+    elseif love.keyboard.isDown("left") then
       posX = posX - 200*dt
 
       -- borders to the window
@@ -195,14 +274,6 @@ function love.update(dt)
       end
 
       objects.player.body:setX(posX)
-
-      for k, v in ipairs(coinsToLoad) do
-        objects.coins[v].body:release()
-        loadNewCoinLocation(v)
-        table.remove(coinsToLoad, 1)
-      end
-
-      
     end
   end
 end
@@ -214,7 +285,7 @@ function love.draw()
   if gameState == "start" or gameState == "end" then
     love.graphics.setColor(0, 0, 0)
     love.graphics.print("Press Space to Start", 300, 325)
-    
+
     love.graphics.setColor(153/255, 147/255, 178/255)
     love.graphics.polygon("fill", 20, 20, 20, 150, 120, 150, 120, 20)
 
@@ -223,7 +294,7 @@ function love.draw()
 
     love.graphics.setColor(229/255, 242/255, 243/255)
     love.graphics.print(objects.player.score, 80, 30)
-    
+
     for k,v in ipairs(objects.enemies) do
       love.graphics.setColor(166/255, 66/255, 83/255)
       love.graphics.print(v.name, 23, 30 + k*20)
@@ -231,31 +302,31 @@ function love.draw()
       love.graphics.setColor(229/255, 242/255, 243/255)
       love.graphics.print(v.score, 80, 30 + k*20)
     end
-    
-    
-    
+
+
+
     if gameState == "start" then
-        love.graphics.setColor(166/255, 66/255, 83/255)
-        love.graphics.print("Write Your Name. It will appear on the scoreboard", 275, 375)
+      love.graphics.setColor(166/255, 66/255, 83/255)
+      love.graphics.print("Write Your Name. It will appear on the scoreboard.\n Press Enter to confirm and enter lobby.", 275, 375)
     end
-    
+
   else
     --this puts the world into motion
 
-    love.graphics.setColor(63/255, 57/255, 65/255) 
+    love.graphics.setColor(63/255, 57/255, 65/255)
     love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
 
-    love.graphics.setColor(164/255, 110/255, 115/255) 
+    love.graphics.setColor(164/255, 110/255, 115/255)
     love.graphics.polygon("fill", objects.player.body:getWorldPoints(objects.player.shape:getPoints()))
 
     for k, v in ipairs(objects.platforms) do
-      love.graphics.setColor(86/255, 78/255, 88/255) 
+      love.graphics.setColor(86/255, 78/255, 88/255)
       love.graphics.polygon("fill", v.body:getWorldPoints(v.shape:getPoints()))
     end
 
     for k, v in ipairs(objects.coins) do
       love.graphics.setColor(250/255, 166/255, 19/255)
-      love.graphics.circle("fill", v.body:getX(),v.body:getY(), v.shape:getRadius() )
+      love.graphics.circle("fill", v.x,v.y, v.r )
     end
 
     love.graphics.setColor(153/255, 147/255, 178/255)
@@ -266,9 +337,16 @@ function love.draw()
 
     love.graphics.setColor(229/255, 242/255, 243/255)
     love.graphics.print(objects.player.score, 80, 30)
-    
+
     love.graphics.setColor(166/255, 66/255, 83/255)
     love.graphics.print(math.floor(missing), 600 , 30)
 
+    for k,v in ipairs(objects.enemies) do
+      love.graphics.setColor(166/255, 66/255, 83/255)
+      love.graphics.print(v.name, 23, 30 + k*20)
+
+      love.graphics.setColor(229/255, 242/255, 243/255)
+      love.graphics.print(v.score, 80, 30 + k*20)
+    end
   end
 end
